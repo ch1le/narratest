2d1d39316e73aa10d236f2d64b9d4c0b436ca9c0
ch1le <55945561+ch1le@users.noreply.github.com>
2025-06-22 11:05:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ function drawRoute(list) {
   }
 }
 
-/* ---------- Content Bar */ ---------- */
+/* ---------- Content Bar ---------- */
 function updateContentBar(count = null) {
   const b = map.getBounds();
   let vis = chain.filter((t, i) => b.contains([t.lat, t.lon]));
